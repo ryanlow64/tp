8fdbda74e75c1a353788ca617abe80f13c14cc93
Chen Xu <chenxu209052@gmail.com>
2025-03-17 19:03:40 +0800

    Fix issue where JSON file could not be properly read

--- src/main/java/seedu/address/model/commons/Address.java
+++ src/main/java/seedu/address/model/commons/Address.java
@@ -34,7 +34,7 @@ public class Address {
      * Returns true if a given string is a valid email.
      */
     public static boolean isValidAddress(String test) {
-        return test == "(blank)" || test.matches(VALIDATION_REGEX);
+        return test.equals("(blank)") || test.matches(VALIDATION_REGEX);
     }
 
     @Override

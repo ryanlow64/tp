5352b47e06cc0da6f2d8dba34e59c0aac244143f
Chen Xu <chenxu209052@gmail.com>
2025-03-26 14:46:26 +0800

    Update DeleteClientCommand
    User will not be able to delete contact details of clients should they
    be involved in property listings, deals, or any events

--- src/main/java/seedu/address/logic/commands/client/DeleteClientCommand.java
+++ src/main/java/seedu/address/logic/commands/client/DeleteClientCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands.client;
 
 import static java.util.Objects.requireNonNull;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
@@ -12,6 +13,11 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.client.Client;
+import seedu.address.model.client.ClientName;
+import seedu.address.model.deal.Deal;
+import seedu.address.model.deal.DealStatus;
+import seedu.address.model.event.Event;
+import seedu.address.model.property.Property;
 
 /**
  * Deletes a client identified using it's displayed index from the address book.
@@ -34,15 +40,67 @@ public class DeleteClientCommand extends DeleteCommand<Client> {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Client> lastShownList = model.getFilteredClientList();
+        List<Client> lastShownClientList = model.getFilteredClientList();
+        List<Deal> lastShownDealList = model.getFilteredDealList();
+        List<Event> lastShownEventList = model.getFilteredEventList();
+        List<Property> lastShownPropertyList = model.getFilteredPropertyList();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+        if (targetIndex.getZeroBased() >= lastShownClientList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX);
         }
 
-        Client clientToDelete = lastShownList.get(targetIndex.getZeroBased());
+        Client clientToDelete = lastShownClientList.get(targetIndex.getZeroBased());
+
+        if (existInDeal(clientToDelete, lastShownDealList)
+            || existInEvent(clientToDelete, lastShownEventList)
+            || existInProperty(clientToDelete, lastShownPropertyList)) {
+            throw new CommandException("Cannot delete");
+        }
+
         model.deleteClient(clientToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_CLIENT_SUCCESS, Messages.formatClient(clientToDelete)));
+    }
+
+    private boolean existInDeal(Client clientToDelete, List<Deal> dealList) {
+        boolean isInDeal = false;
+        ClientName clientNameToDelete = clientToDelete.getClientName();
+        for (Deal deal : dealList) {
+            if (clientNameToDelete.equals(deal.getBuyer()) || clientNameToDelete.equals(deal.getSeller())) {
+                if (deal.getStatus() == DealStatus.CLOSED) {
+                    continue;
+                }
+                isInDeal = true;
+                break;
+            }
+        }
+        return isInDeal;
+    }
+
+    private boolean existInEvent(Client clientToDelete, List<Event> eventList) {
+        boolean isInEvent = false;
+        ClientName clientNameToDelete = clientToDelete.getClientName();
+        for (Event event : eventList) {
+            if (clientNameToDelete.equals(event.getClientName())) {
+                if (LocalDateTime.now().isAfter(event.getDateTime())) {
+                    continue;
+                }
+                isInEvent = true;
+                break;
+            }
+        }
+        return isInEvent;
+    }
+
+    private boolean existInProperty(Client clientToDelete, List<Property> propertyList) {
+        boolean isInProperty = false;
+        ClientName clientNameToDelete = clientToDelete.getClientName();
+        for (Property property : propertyList) {
+            if (clientNameToDelete.equals(property.getOwner())) {
+                isInProperty = true;
+                break;
+            }
+        }
+        return isInProperty;
     }
 
     @Override

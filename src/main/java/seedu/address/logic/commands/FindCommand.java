2ebf3375d1bbe06ac4821519d85ef2373b2bda9e
Kumar2215 <kumar.kg279@gmail.com>
2025-03-26 19:14:43 +0800

    Fix abstract FindCommand

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -2,24 +2,25 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.function.Predicate;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.client.NameContainsKeywordsPredicate;
 
 /**
  * Finds and lists all clients in address book whose name contains any of the argument keywords.
  * Keyword matching is case insensitive.
  */
-public abstract class FindCommand extends Command {
+public abstract class FindCommand<T> extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    protected final NameContainsKeywordsPredicate predicate;
+    protected final Predicate<T> predicate;
 
     /**
      * Creates an FindCommand to find the specified item.
      */
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
+    public FindCommand(Predicate<T> predicate) {
         requireNonNull(predicate);
         this.predicate = predicate;
     }
